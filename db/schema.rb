be01b438ad2f6e401468da221718ac0b5ffdda11
Katie Frances <katiebeeisdead@gmail.com>
2018-05-05 12:11:03 +1000

    Fixed user id issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_04_073821) do
+ActiveRecord::Schema.define(version: 2018_05_05_020859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2018_05_04_073821) do
     t.integer "shipping"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
