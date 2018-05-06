e6cc9beda3bd62c73a3223791e983b1b0c5bd049
Katie Frances <katiebeeisdead@gmail.com>
2018-05-06 13:35:57 +1000

    Add scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "listings#index"
   resources :profiles
   resources :listings
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
