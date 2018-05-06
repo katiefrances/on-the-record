0cdd1590071fb3b15c6bf6cef31ee0f6c8e3b8c0
Katie Frances <katiebeeisdead@gmail.com>
2018-05-07 09:42:02 +1000

    Log in/Log out functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_05_020859) do
+ActiveRecord::Schema.define(version: 2018_05_06_034019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 2018_05_05_020859) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "avatar"
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
