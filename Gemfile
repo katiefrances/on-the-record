4550ab5e7cf1d38361155e00a4cbbd8015e31d3d
Katie Frances <katiebeeisdead@gmail.com>
2018-05-07 19:31:33 +1000

    Add Stripe

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'figaro'
 
 gem "fog-aws"
 
+gem 'stripe'
+
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :profiles
   devise_for :users
   resources :listings
+  resources :charges
   root to: "listings#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
