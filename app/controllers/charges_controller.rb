1860416210b4b52be9f1277dd0cdd84cb163039e
Katie Frances <katiebeeisdead@gmail.com>
2018-05-08 15:40:08 +1000

    Stripe Config

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,11 +1,11 @@
 class ChargesController < ApplicationController
-
+  before_action :set_item
     def new
     end
     
     def create
       # Amount in cents
-      @amount = 500
+      @amount = @listing.price*100
     
       customer = Stripe::Customer.create(
         :email => params[:stripeEmail],
@@ -16,12 +16,15 @@ class ChargesController < ApplicationController
         :customer    => customer.id,
         :amount      => @amount,
         :description => 'Rails Stripe customer',
-        :currency    => 'usd'
+        :currency    => 'aud'
       )
     
     rescue Stripe::CardError => e
       flash[:error] = e.message
       redirect_to new_charge_path
     end
-
-end
+    private
+      def set_item
+        @listing = Listing.find(params[:listing_id])
+      end
+ end
