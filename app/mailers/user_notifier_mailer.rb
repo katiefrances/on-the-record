dcb634863c006f7416fcd6bdaba77df2f54b814a
Katie Frances <katiebeeisdead@gmail.com>
2018-05-09 13:39:54 +1000

    Fix Sendgrid Message

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,17 +1,9 @@
 class UserNotifierMailer < ApplicationMailer
-
-        default :from => 'admin@ontherecord.com'
-      
-        # send a signup email to the user, pass in the user object that   contains the user's email address
-        def send_signup_email(recipient)
-          mail( :to => recipient.email,
-          :subject => 'Thanks for signing up for our amazing app' )
-        end
+    default :from => 'admin@ontherecord.com'
+  
+    # send a signup email to the user, pass in the user object that   contains the user's email address
+    def send_signup_email(recipient)
+      mail( :to => recipient.email,
+      :subject => 'New Listing Added' )
+    end
 end
-
-
-# def send_signup_email(user)
-#   @user = user
-#   mail( :to => @user.email,
-#   :subject => 'Thanks for signing up for our amazing app' )
-# end
