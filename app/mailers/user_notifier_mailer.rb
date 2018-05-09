7681c6c3177f5a317fbd16b49c599fc9a8a5bb43
Katie Frances <katiebeeisdead@gmail.com>
2018-05-09 11:55:04 +1000

    Sendgrid

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,10 +1,11 @@
 class UserNotifierMailer < ApplicationMailer
 
-        default :from => 'any_from_address@example.com'
+        default :from => 'admin@ontherecord.com'
       
         # send a signup email to the user, pass in the user object that   contains the user's email address
-        def send_signup_email
-          mail( :to => "katiebeeisdead@gmail.com",
+        def send_signup_email(user)
+          @user = user
+          mail( :to => @user,
           :subject => 'Thanks for signing up for our amazing app' )
         end
 end
