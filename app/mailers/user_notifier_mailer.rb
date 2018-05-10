d7c3c223093d42db99cfa817741ea98e581d1071
Katie Frances <katiebeeisdead@gmail.com>
2018-05-10 18:37:45 +1000

    Fix

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,13 +1,5 @@
 class UserNotifierMailer < ApplicationMailer
     default :from => 'admin@ontherecord.com'
-<<<<<<< HEAD
-    # send an email to the user, pass in the recipient object that contains the user's email address
-    def send_signup_email(recipient)
-      mail( :to => recipient.email,
-      :subject => 'Thanks for signing up for our amazing app' )
-    end
-end
-=======
   
     # send a signup email to the user, pass in the user object that   contains the user's email address
     def send_signup_email(recipient)
@@ -15,4 +7,3 @@ end
       :subject => 'New Listing Added' )
     end
 end
->>>>>>> styling
